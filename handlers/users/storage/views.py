fe99288852d5e8e3939a879d60fa692b0cb27eec
Dima <dima.trotsiuk322@gmail.com>
2022-04-18 21:33:47 +0200

    Added realizations of the "Склад" buttons

---
+++ handlers/users/storage/views.py
@@ -0,0 +1,38 @@
+from aiogram import types
+import requests
+import json
+
+from data.config import IP
+from loader import dp
+
+
+@dp.message_handler(text=["Склад Дімона", "Склад Владоса"])
+async def get_storage_dima_vlad(message: types.Message):
+    product_info_list = requests.get(f'{IP}/api/v1/product_info_list/')
+
+    product_info_list = json.loads(product_info_list.text)
+    flag = {'Склад Дімона': 'quantity_dima', 'Склад Владоса': 'quantity_vlad'}
+    user = flag[message.text]
+
+    if message.text == 'Склад Дімона':
+        text = 'Склад Дмутра ☺️\n\n'
+    else:
+        text = 'Склад Владоса ☺️\n\n'
+
+    for product_info in product_info_list:
+        text += f"<i>'{product_info['title']}'</i> - "
+        text += f"<b>{product_info['storage_stickers'][user]}</b>шт\n"
+    await message.answer(text)
+
+
+@dp.message_handler(text="Фулл склад")
+async def get_storage_full(message: types.Message):
+    product_info_list = requests.get(f'{IP}/api/v1/product_info_list/')
+
+    product_info_list = json.loads(product_info_list.text)
+    text = 'Фулл склад 😓️\n\n'
+
+    for product_info in product_info_list:
+        text += f"<i>'{product_info['title']}'</i> - "
+        text += f"<b>{product_info['total_quantity']}</b>шт\n"
+    await message.answer(text)
---
+++ keyboards/default/menu.py
@@ -0,0 +1,21 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+default_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Склад Дімона"),
+            KeyboardButton(text="Склад Владоса"),
+            KeyboardButton(text="Фулл склад"),
+        ],
+        [
+            KeyboardButton(text="Редагувати склад"),
+            KeyboardButton(text="Управління замовленнями"),
+            KeyboardButton(text="Корзина"),
+        ],
+        [
+            KeyboardButton(text="Пошук по ТТН"),
+            KeyboardButton(text="Статистика"),
+        ],
+    ],
+    resize_keyboard=True
+)
