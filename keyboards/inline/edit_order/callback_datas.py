8c402c0243c3d7ab17aac54fd2d65c957020a985
Dima <dima.trotsiuk322@gmail.com>
2022-04-20 12:54:45 +0200

    Init

---
+++ keyboards/inline/edit_order/callback_datas.py
@@ -0,0 +1,3 @@
+from aiogram.utils.callback_data import CallbackData
+
+show_buttons_callback = CallbackData("show_buttons", "type_command", "action")
---
+++ keyboards/inline/edit_order/show_buttons.py
@@ -0,0 +1,34 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from .callback_datas import show_buttons_callback
+
+
+async def show_buttons_order():
+    list_button = [
+        [
+            InlineKeyboardButton(
+                text=f"Створити",
+                callback_data=show_buttons_callback.new(action="create", type_command='show_buttons_order')
+            ),
+            InlineKeyboardButton(
+                text=f"Переглянути",
+                callback_data=show_buttons_callback.new(action="view", type_command='show_buttons_order')
+            ),
+            InlineKeyboardButton(
+                text=f"Відправити",
+                callback_data=show_buttons_callback.new(action="send", type_command='show_buttons_order')
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"Редагувати",
+                callback_data=show_buttons_callback.new(action="edit", type_command='show_buttons_order')
+            ),
+            InlineKeyboardButton(
+                text=f"Видалити",
+                callback_data=show_buttons_callback.new(action="delete", type_command='show_buttons_order')
+            ),
+        ]
+    ]
+
+    return InlineKeyboardMarkup(row_width=1, inline_keyboard=list_button)
