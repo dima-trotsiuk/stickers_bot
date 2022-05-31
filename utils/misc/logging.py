984bfaa5fcba90d0c7a77f063ee4480c52223bd8
dima-trotsiuk <72335974+dima-trotsiuk@users.noreply.github.com>
2022-05-31 09:54:24 +0200

    Delete logging.py

--- utils/misc/logging.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig(filename='app.log', filemode='w',
-                    format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level=logging.INFO,
-                    )
